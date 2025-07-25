26fa601862c7c58fecfbbe590e23e0b94fe8b2fd
wealipay <1293600794@qq.com>
2025-07-25 23:04:11 +0800

    ghhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import LightRays from "./reactbits/LightRays";
 export default function Home() {
+  const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
+
   return (
-<SplitText
-  text="Hello, GSAP!"
-  className="text-2xl font-semibold text-center"
-  delay={100}
-  duration={0.6}
-  ease="power3.out"
-  splitType="chars"
-  from={{ opacity: 0, y: 40 }}
-  to={{ opacity: 1, y: 0 }}
-  threshold={0.1}
-  rootMargin="-100px"
-  textAlign="center"
-  onLetterAnimationComplete={handleAnimationComplete}
-/>
+    <SplitText
+      text="Hello, GSAP!"
+      className="text-2xl font-semibold text-center"
+      delay={100}
+      duration={0.6}
+      ease="power3.out"
+      splitType="chars"
+      from={{ opacity: 0, y: 40 }}
+      to={{ opacity: 1, y: 0 }}
+      threshold={0.1}
+      rootMargin="-100px"
+      textAlign="center"
+      onLetterAnimationComplete={handleAnimationComplete}
+    />
   );
 }
