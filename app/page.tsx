34e4720015b7a757619c29cd61e9d8b4b78c4c29
wealipay <1293600794@qq.com>
2025-07-25 23:03:15 +0800

    ghhh

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import LightRays from "./reactbits/LightRays";
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "600px", position: "relative" }}>
-      <LightRays
-        raysOrigin="top-center"
-        raysColor="#00ffff"
-        raysSpeed={1.5}
-        lightSpread={0.8}
-        rayLength={1.2}
-        followMouse={true}
-        mouseInfluence={0.1}
-        noiseAmount={0.1}
-        distortion={0.05}
-        className="custom-rays"
-      />
-    </div>
+<SplitText
+  text="Hello, GSAP!"
+  className="text-2xl font-semibold text-center"
+  delay={100}
+  duration={0.6}
+  ease="power3.out"
+  splitType="chars"
+  from={{ opacity: 0, y: 40 }}
+  to={{ opacity: 1, y: 0 }}
+  threshold={0.1}
+  rootMargin="-100px"
+  textAlign="center"
+  onLetterAnimationComplete={handleAnimationComplete}
+/>
   );
 }
