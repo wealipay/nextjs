ef0e59a29e8d2e703797cc5c1966f37ab98e567c
wealipay <1293600794@qq.com>
2025-07-25 21:31:51 +0800

    hhhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
 import React from "react";
+import LightRays from "@/reactbits/LightRays";
 export default function Home() {
-  return <div>hello</div>;
+  return <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <LightRays
+    raysOrigin="top-center"
+    raysColor="#00ffff"
+    raysSpeed={1.5}
+    lightSpread={0.8}
+    rayLength={1.2}
+    followMouse={true}
+    mouseInfluence={0.1}
+    noiseAmount={0.1}
+    distortion={0.05}
+    className="custom-rays"
+  />
+</div>;
 }
