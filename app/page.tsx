08e38fb4a802170b6eabae7110704c870b1110f1
wealipay <1293600794@qq.com>
2025-08-07 10:00:08 +0800

    ggh

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 export default function Home() {
-  const handleAnimationComplete = () => {
-    console.log("All letters have animated!");
-  };
 
   return <div>he</div>;
 }
