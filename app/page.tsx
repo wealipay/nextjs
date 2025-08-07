24eae2adde118b14d098fcb92442247024d75e2e
wealipay <1293600794@qq.com>
2025-08-07 09:52:40 +0800

    ggh

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,6 @@ export default function Home() {
   };
 
   return (
-    <SplitText
-      text="Hello, GSAP!f"
-      className="text-2xl font-semibold text-center"
-      delay={100}
-      duration={0.6}
-      ease="power3.out"
-      splitType="chars"
-      from={{ opacity: 0, y: 40 }}
-      to={{ opacity: 1, y: 0 }}
-      threshold={0.1}
-      rootMargin="-100px"
-      textAlign="center"
-      onLetterAnimationComplete={handleAnimationComplete}
-    />
+    <div>he</div>
   );
 }
