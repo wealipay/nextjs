3635547b4907ae21c0e263c8f45f4e7b5e64e63d
wealipay <1293600794@qq.com>
2025-08-08 10:31:29 +0800

    ghh

---
+++ app/[productId]/page.tsx
@@ -0,0 +1,8 @@
+export default async function ProductDetail({
+  params
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const productId = (await params).productId;
+  return <div>{productId}</div>
+}
