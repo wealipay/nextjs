252efdee6fb42f1ae76709fd058f2ba93271812e
wealipay <1293600794@qq.com>
2025-08-17 04:10:29 +0800

    hhh

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,15 +1,4 @@
-import  { Metadata } from "next";
-type Props = {
-  params: { productId: string };
-};
-export const generateMetadata = async ({
-  params
-}: Props): Metadata => {
-  const productId = (await params).productId;
-  return {
-    title: `产品id ${productId}`
-  };
-};
+
 export default async function ProductDetail({
   params
 }: {
