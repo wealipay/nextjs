893df700b2235184e3a4420ea9ef167e276b1029
wealipay <1293600794@qq.com>
2025-09-19 22:02:52 +0800

    hhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 export default function Home() {
-  return <div className="flex justify-center items-center">
-    <div></div>
-  </div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="">五一</div>
+    </div>
+  );
 }
