1944448bf00a463b2779421f3124e2e23eded780
wealipay <1293600794@qq.com>
2025-09-19 23:32:46 +0800

    hhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import hao from "@/assets/images/hao.png";
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
@@ -7,7 +8,7 @@ export default function Home() {
         <h2 className="font-bold text-3xl text-primary-text">登陆</h2>
         <button className="bg-white w-full mt-8 h-10 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-200 rounded-full">
           <Image
-            src="@/assets/images/hao.png"
+            src={hao}
             alt="薅羊毛"
             width={470}
             height={470}
