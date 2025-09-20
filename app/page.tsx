c38213867cf49445763a8fe644ac2a713e5901ac
wealipay <1293600794@qq.com>
2025-09-20 22:02:10 +0800

    ggg

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="max-w-[300px] w-[95%] py-12 rounded-lg">
-        <h2 className="font-bold text-center text-3xl text-primary-text">选择登陆方式</h2>
+        <h2 className="font-bold text-center text-3xl text-primary-text">
+          选择登陆方式
+        </h2>
         <button className="bg-white w-full mt-8 h-10 flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-200 rounded-full">
           <Image
             src={hao}
@@ -24,7 +26,7 @@ export default function Home() {
         <input
           type="text"
           placeholder="手机，账号，邮箱"
-          className="w-full bg-background outline-none rounded-md p-4 placeholder-secondary-text border border-border"
+          className="w-full bg-background outline-none rounded-md p-4 placeholder-primary-text text-primary-text border border-border"
         />
         <button className="flex justify-center items-center w-full font-medium rounded-full">
           提交
